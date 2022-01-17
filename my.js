418ee2886c109c55ee093a6a05205ab61a0d4545
Azizur Rahman <97599569+Azizur-R@users.noreply.github.com>
2022-01-17 17:22:14 +0600

    Add files via upload

---
+++ my.js
@@ -0,0 +1,16 @@
+
+
+
+var firstName = "Abdul";
+var lastName  = "kader";
+var age       = 22;
+
+document.write(firstName, " ",lastName, " is ", age, " years old .....!")
+
+
+
+var a = 5;
+ var b = 62;
+ var c = b%a;
+ 
+ document.write(c);
